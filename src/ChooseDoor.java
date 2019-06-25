39be7bf24c11f85a54c45539a8b1da7f0450e081
Vinicius Oliveira <vr.sena@gmail.com>
2019-06-25 14:19:23 -0400

    Update ChooseDoor.java

--- src/ChooseDoor.java
+++ src/ChooseDoor.java
@@ -1,15 +1,51 @@
-//import java.util.Random;
+import java.util.Scanner;
 
 public class ChooseDoor {
 	
     public static void main(String[] args) {
-    	// boolean blnWin = false;
+
+         
+    	Scanner scan = new Scanner(System.in);
+        System.out.println("Enter how many games:"); 
+        int intNumberOfGames = scan.nextInt();
+        System.out.println("Play " + intNumberOfGames + " times.");
+        
+        scan.close();
+        
+        if (intNumberOfGames < 1) { return; }
+        
+        int intSwitchAndWin = 0;
+        int intNotSwitchAndWin = 0;
+        
+        for (int i = 0; i < intNumberOfGames; i++) {
+        	boolean gameResult[] = playGame();
+        	
+        	if (gameResult[0] && gameResult[1]) {
+        		intSwitchAndWin++;
+        	} else if (!gameResult[0] && gameResult[1]) {
+        		intNotSwitchAndWin++;
+        	}
+        }
+        
+        double ratioSwitchAndWin = (double)intSwitchAndWin / (double)intNumberOfGames;
+//        double ratioNotSwitchAndWin = (double)intNotSwitchAndWin / (double)intNumberOfGames;
+        
+        System.out.println("Ratio for switching and winning: " + ratioSwitchAndWin);
+//        System.out.println("Ratio for not switching and winning: " + ratioNotSwitchAndWin);
+        
+    }
+    
+    // Plays Monty Hall's game and returns: [Switch or Not , Win or Not]
+    public static boolean[] playGame() {
+        boolean[] result = new boolean[2];
+
+        boolean blnWin = false;
     	int intNumberOfDoors = 3;
     	
         int intCarDoor = (int)(Math.random() * intNumberOfDoors + 1); // Door where the car is
         int intChoice = (int)(Math.random() * intNumberOfDoors + 1); // Door chosen by the player
-        System.out.println("Car: " + intCarDoor);
-        System.out.println("Choice: " + intChoice);
+        // System.out.println("Car: " + intCarDoor);
+        // System.out.println("Choice: " + intChoice);
         
         // Door revealed
         int intDoorRevealed = 0;
@@ -18,7 +54,7 @@ public class ChooseDoor {
         		intDoorRevealed = i;
         	}
         }
-        System.out.println("Revealed: " + intDoorRevealed);
+        // System.out.println("Revealed: " + intDoorRevealed);
         
         // The player decides to switch or not
         boolean blnSwitch = getRandomBoolean();
@@ -31,19 +67,22 @@ public class ChooseDoor {
             }
         }
     	
-    	System.out.println("Switch? " + blnSwitch);
-    	System.out.println("Choice: " + intChoice);
+    	// System.out.println("Switch? " + blnSwitch);
+    	// System.out.println("Choice: " + intChoice);
     	
     	// Check if the player made the right choice
     	if (intChoice == intCarDoor) {
-    		// blnWin = true;
-    		System.out.println("You win!");
-    	} else {
-    		System.out.println("You lose...");
-    	}
+    		blnWin = true;
+    		// System.out.println("You win!");
+        } 
+        // else {
+    	// 	System.out.println("You lose...");
+        // }
+        
+    	result[0] = blnSwitch;
+    	result[1] = blnWin;
+        return result;
     }
-    
-    
     
     // Returns a random boolean
     public static boolean getRandomBoolean() {
